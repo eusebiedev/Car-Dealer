f4c2cb8c5f1c697b28ba60b60167d27f75e7f655
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-09 14:17:46 -0700

    Add Http Get, Post logic to controller file
    Co-Authored-By: Marcus Kyung <Kyungmj@gmail.com>

--- CarDealer/Controllers/HomeController.cs
+++ CarDealer/Controllers/HomeController.cs
@@ -1,20 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using CarDealer.Models;
+using System.Collections.Generic;
 
 namespace CarDealer.Controllers
 {
   public class HomeController : Controller
   {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List<Car> allCars = Car.GetAll();
+      return View(allCars);
+    }
 
+    [HttpGet("/cars/new")]
+    public ActionResult CarForm()
+    {
+      return View();
+    }
 
+    [HttpPost("/cars")]
+    public ActionResult Create(string carType, int price, int miles)
+    {
+      Car myCar = new Car(carType, price, miles);
+      return RedirectToAction("Index");
+    }
     
   }
-
-
-
-
-
-
-
-
 }
