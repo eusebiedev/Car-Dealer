8fd7f840816043bf5825daf5f24cc9a9c2ebe90a
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-09 10:47:53 -0700

    Write 2nd test to add carType to list, Pass
    Co-Authored-By: Marcus Kyung <Kyungmj@gmail.com>

--- CarDealer.Tests/ModelTests/CarTests.cs
+++ CarDealer.Tests/ModelTests/CarTests.cs
@@ -16,8 +16,20 @@ namespace CarDealer.Tests
     [TestMethod]
     public void CarConstructor_CreatesInstanceOfCar_Car()
     {
-      Car newCar = new Car();
+      Car newCar = new Car("Corolla");
       Assert.AreEqual(typeof(Car), newCar.GetType());
+    }
+
+    [TestMethod]
+    public void GetCarType_ReturnsCarType_String()
+    {
+      //Arrange
+      string carType = "Corolla";
+      //Act
+      Car newCar = new Car(carType);
+      string result = newCar.CarType;
+      //Assert
+      Assert.AreEqual(carType, result);
     }
   }
 }
