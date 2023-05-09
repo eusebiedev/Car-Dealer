96a2f6f16656907eed9a0041c6a977249df8c870
Eusebie Siebenberg <siebenee@gmail.com>
2023-05-09 10:48:42 -0700

    Add logic to CarDealer.cs to create list type
    Co-Authored-By: Marcus Kyung <Kyungmj@gmail.com>

--- CarDealer/Models/CarDealer.cs
+++ CarDealer/Models/CarDealer.cs
@@ -1,10 +1,26 @@
+using System.Collections.Generic;
+
 namespace CarDealer.Models
 {
-  public class CarDealerVariable
+  public class Car
   {
-    
+    public string CarType { get; set; }
+    private static List<Car> _instances = new List<Car> { };
 
+    public Car(string carType)
+    {
+      CarType = carType;
+      _instances.Add(this);
+    }
 
+    public static List<Car> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
   }
-
 }
